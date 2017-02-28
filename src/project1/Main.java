ddeb77701656932f5576ecae6c6d7aa86cc0f435
tariq4321 <tariq.siddiqui.53@gmail.com>
2017-02-28 16:26:49 -0500

    Added hashMap to include table identity in columns to fetch and while saving column indexes

--- src/project1/Main.java
+++ src/project1/Main.java
@@ -22,6 +22,7 @@ import net.sf.jsqlparser.expression.operators.arithmetic.Addition;
 import net.sf.jsqlparser.expression.operators.arithmetic.Multiplication;
 import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
 import net.sf.jsqlparser.expression.operators.conditional.OrExpression;
+import net.sf.jsqlparser.expression.operators.relational.EqualsTo;
 import net.sf.jsqlparser.expression.operators.relational.GreaterThan;
 import net.sf.jsqlparser.parser.CCJSqlParser;
 import net.sf.jsqlparser.parser.ParseException;
@@ -121,9 +122,9 @@ public class Main{
 	public static Scanner scan;
 	public static CCJSqlParser parser;
 	public static PlainSelect plain;
-	public static int columnIndexesToFetchInSelectStatement[];
 	public static ArrayList <String> columnDataTypes = new ArrayList <String>();
-	public static Map<String,Integer> columnNameToIndexMap = null;
+	public static Map<String,Map<String,Integer>> columnIndexesInTable = new HashMap<String,Map<String,Integer>>();
+	public static Map<String,int[]> columnsToFetch = new HashMap<String,int []>();
 	public static Select select;
 	public static SelectBody body;
 
@@ -186,7 +187,7 @@ public class Main{
 
 	public static void initialiseDataStructures()
 	{
-		columnIndexesToFetchInSelectStatement = null;
+		//columnIndexesToFetchInSelectStatement = null;
 		//columnDataTypes = null;  //create table is one command
 		//columnNameToIndexMap = null;
 		csvFile = "src\\project1\\data\\";
@@ -231,6 +232,8 @@ public class Main{
 		List<Expression> el=null;
 		if(ex instanceof OrExpression || ex instanceof AndExpression)
 			el = ExpressionVisitorBase.getChildren(ex);
+		//if(ex instanceof  EqualsTo || ex instanceof EqualsTo)
+//			//el = ExpressionVisitorBase.getChildren(ex);
 		else 
 			el = null;
 		if(el!=null){
@@ -265,9 +268,9 @@ public class Main{
 		//Get Table
 		Table table = (Table) plain.getFromItem();
 		String tableName = table.getName();
-		csvFile = csvFile+tableName+".csv";
+		String csvFile_local_copy = csvFile+tableName+".csv";
 		//System.out.println(csvFile);
-		br = new BufferedReader(new FileReader(String.format(csvFile)));
+		br = new BufferedReader(new FileReader(String.format(csvFile_local_copy)));
 
 
 		while ((line = br.readLine()) != null) {
@@ -275,11 +278,11 @@ public class Main{
 			// use | as separator
 			String[] ColumnsInSelectStatement = line.split("\\|");
 
-			for(int i=0;i<columnIndexesToFetchInSelectStatement.length-1;i++)
+			for(int i=0;i<columnsToFetch.get(tableName).length-1;i++)
 			{
-				System.out.print(ColumnsInSelectStatement[columnIndexesToFetchInSelectStatement[i]]+"|");
+				System.out.print(ColumnsInSelectStatement[columnsToFetch.get(tableName)[i]]+"|");
 			}
-			System.out.print(ColumnsInSelectStatement[columnIndexesToFetchInSelectStatement[columnIndexesToFetchInSelectStatement.length-1]]+"\n");
+			System.out.print(ColumnsInSelectStatement[columnsToFetch.get(tableName)[columnsToFetch.get(tableName).length-1]]+"\n");
 		}
 	}
 
@@ -287,16 +290,21 @@ public class Main{
 	/* get all columns to fetch as per Select Statement*/
 	public static void findcolumnsToFetchInSelectStatement()
 	{
+	
+		
 		plain = (PlainSelect)body;
+		Table table = (Table) plain.getFromItem();
+		String tableName = table.getName();
 		List<SelectItem> si =  plain.getSelectItems();
 		System.out.println(plain.getFromItem());
 		ListIterator<SelectItem> it = si.listIterator();
-
+		int columnIndexesToFetchInSelectStatement[];
 		String str = si.toString();
 		if(str.contains("*"))
 		{
 			//System.out.println("Contains *" + si);
-			columnIndexesToFetchInSelectStatement = new int[columnNameToIndexMap.size()] ;
+			
+			columnIndexesToFetchInSelectStatement = new int[columnIndexesInTable.get(tableName).size()] ; //all columns
 		}
 		else
 		{
@@ -313,7 +321,7 @@ public class Main{
 			if(col_name.equals("*"))  //fetch all the columns
 			{
 				//put all column names in fetch list
-				for (Map.Entry<String, Integer> entry : columnNameToIndexMap.entrySet()) {
+				for (Map.Entry<String, Integer> entry : columnIndexesInTable.get(tableName).entrySet()) {
 					//String key = entry.getKey();
 					Integer col_index = entry.getValue();
 					// System.out.println("value = " +value);
@@ -326,10 +334,12 @@ public class Main{
 			else
 			{
 				//System.out.println("Corresponding Column Index = " + columnNameToIndexMap.get(col_name));
-				columnIndexesToFetchInSelectStatement[i]=columnNameToIndexMap.get(col_name); //save indexes of all columns to fetch
+				columnIndexesToFetchInSelectStatement[i]=columnIndexesInTable.get(tableName).get(col_name); //save indexes of all columns to fetch
 				i++;
 			}
 		}
+		
+		columnsToFetch.put(tableName,columnIndexesToFetchInSelectStatement);
 	}
 
 
@@ -337,7 +347,10 @@ public class Main{
 	{
 
 		CreateTable create = (CreateTable)statement;
-		columnNameToIndexMap = new HashMap<String,Integer>();
+		String tableName = create.getTable().getName();
+		
+		System.out.println(tableName);
+		Map<String,Integer> columnNameToIndexMap = new HashMap<String,Integer>();
 		List<ColumnDefinition> si = create.getColumnDefinitions();
 		ListIterator<ColumnDefinition> it = si.listIterator();
 
@@ -347,11 +360,12 @@ public class Main{
 			//System.out.println(it.next());
 
 			ColumnDefinition cd = it.next();
-			Evallib e = new Evallib(cd);
+			//Evallib e = new Evallib(cd);
 			columnDataTypes.add(cd.getColDataType().toString());
 			System.out.println("type = "+ columnDataTypes.get(i));
 			columnNameToIndexMap.put(cd.getColumnName() ,i++);
 		}
+		columnIndexesInTable.put(tableName,columnNameToIndexMap);
 	}
 
 
@@ -360,198 +374,10 @@ public class Main{
 
 		StringReader input = new StringReader(temp);
 		parser = new CCJSqlParser(input);
-
 		statement = parser.Statement();    
 	}
 	
 	
-	// Returns true if 'op2' has higher or same precedence as 'op1',
-	// otherwise returns false.
-	public static boolean hasPrecedence(String op1, String op2)
-	{
-		if (op2.equals("(") || op2.equals(")"))
-			return false;
-		if ((op1.equals("*") || op1.equals("/")) && (op2.equals("+")|| op2.equals("-")))
-			return false;
-		else
-			return true;
-	}
-	
-	public static void getWhereConditionList(String Expression) throws IOException
-	{
-		//postfix notation
-		//http://introcs.cs.princeton.edu/java/43stack/Evaluate.java.html
-		//http://codinghelmet.com/?path=exercises/expression-evaluator
-		//http://www.geeksforgeeks.org/expression-evaluation/
-
-
-		System.out.println("After replacing  " + Expression);
-		System.out.println("length = " + Expression.length());
-
-		int index =0;
-		Stack<String> ops  = new Stack<String>();
-		Stack<String> vals = new Stack<String>();
-		String stack_expression=null;
-		boolean break_flag=false;
-
-		while (index < Expression.length()) {
-			String s = String.valueOf(Expression.charAt(index));
-			//System.out.println("s : "+ s);
-			if(s.equals(" "))
-			{
-				index++;
-				continue;
-			}
-			else if  (s.equals("("))
-			{
-				ops.push("(");
-				System.out.println("operator : (");
-			}
-			else if (s.equals("|"))
-			{
-
-				if((index < Expression.length() -1) && (String.valueOf(Expression.charAt(index+1)).equals("|")))
-				{
-					index++;
-					index++;
-					ops.push("||");
-					System.out.println("operator : ||");
-					continue;
-				}
-				ops.push(s);
-				System.out.println("operator : |");
-			}
-			else if (s.equals("&"))
-			{
-				if((index < Expression.length() -1) && (String.valueOf(Expression.charAt(index+1)).equals("&")))
-				{
-					index++;
-					index++;
-					ops.push("&&");
-					System.out.println("operator : &&");
-					continue;
-				}
-				ops.push(s);
-				System.out.println("operator : &");
-			}
-			else if (s.equals(")")) 
-			{
-				System.out.println("operator : )");
-
-				while (!ops.peek().equals("("))
-				{
-					stack_expression = vals.pop().toString() +  ops.pop() + vals.pop();
-					vals.push(stack_expression);
-
-				}
-				ops.pop();
-				/*
-                if(op.equals("+"))
-                {
-                	stack_expression += vals.pop().toString() +  "+" + v;
-                }
-                else if (op.equals("-"))
-                {
-                	stack_expression += vals.pop().toString() +  "-" + v;
-                }
-                else if (op.equals("*"))
-                {
-                	stack_expression += vals.pop().toString() +  "*" + v;
-                }
-                else if (op.equals("/"))
-                {
-                	stack_expression += vals.pop().toString() +  "/" + v;
-                }
-				 */
-
-			}
-			else if (IsOperator(s)==true)
-			{
-				// While top of 'ops' has same or greater precedence to current
-				// token, which is an operator. Apply operator on top of 'ops'
-				// to top two elements in values stack
-				while (!ops.empty() && hasPrecedence(s, ops.peek()))
-				{
-					stack_expression = vals.pop().toString() +  ops.pop() + vals.pop();
-					vals.push(stack_expression);
-				}
-				System.out.println("operator push : "+ s);
-				ops.push(s);
-
-			}
-			else
-			{
-				int startindex= index;
-
-				int endindex = startindex;
-				String nextchar = String.valueOf(Expression.charAt(endindex+1));
-				//System.out.println("startindex  : "+ startindex + "   nextchar : "+ nextchar);
-				while((endindex < Expression.length()) && (IsOperator(nextchar)==false))
-				{
-
-					if(endindex == Expression.length()-1)
-					{
-						break;
-					}
-					endindex++;
-					//System.out.println("inside loop endindex = "+ endindex);
-					nextchar = String.valueOf(Expression.charAt(endindex+1));
-				}
-
-				//System.out.println("startindex  : "+ startindex + "  " + Expression.charAt(startindex)+ "endindex : " + endindex);
-
-				String operand;
-				if(startindex == endindex)
-				{
-					operand = String.valueOf(Expression.charAt(startindex));  
-				}
-				else
-				{
-					operand = Expression.substring(startindex, endindex).trim();            	
-				}
-				System.out.println("operand to push: "+ operand);
-				vals.push(operand.toString());
-			}
-
-
-			if(index!=0 && ops.isEmpty() && vals.isEmpty())
-			{
-				System.out.println("Break");
-				break;
-			}
-			index++;
-			if(break_flag==true)
-			{
-				break;
-			}
-		}
-
-		while (!ops.empty())
-		{
-			stack_expression = vals.pop().toString() +  ops.pop() + vals.pop();
-			vals.push(stack_expression);
-		}
-
-		stack_expression =vals.pop();
-
-		//System.out.println(vals.pop());
-		System.out.println(stack_expression);
-	}
-	
-	public static boolean IsOperator(String s) throws IOException
-	{
-
-		//System.out.println("s : "+  s);
-		if (s.equals("+") || (s.equals("-")) || (s.equals("*")) || (s.equals("/")) || (s.equals("|")) 
-				|| (s.equals("&")) || (s.equals("==")) || (s.equals("!=")) || (s.equals("=")) || 
-				s.equals(")") || s.equals("("))
-		{
-			//System.out.println("return true  : "+  s);
-			return true;
-		}
-		//System.out.println("return false");
-		return false;
-	}
 
 }
 
